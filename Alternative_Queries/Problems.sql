9a01ce517d0903a4d97cb8d06e2cfc8a0511644c
Hussein Mohamed <hussein7mohamed8@gmail.com>
2025-06-18 00:14:53 +0300

    Update Problems.sql

--- Alternative_Queries/Problems.sql
+++ Alternative_Queries/Problems.sql
@@ -40,3 +40,24 @@ WITH RECURSIVE pattern(n) AS (
 )
 SELECT REPEAT('* ', n) AS line
 FROM pattern;
+
+
+--003- Print Prime Numbers
+/*
+Write a query to print all prime numbers less than or equal to 1000, & as a separator. 2&3&5
+*/
+
+WITH RECURSIVE numbers AS (
+    SELECT 2 AS n
+    UNION ALL
+    SELECT n + 1 FROM numbers WHERE n + 1 <= 1000
+),
+primes AS (
+    SELECT n FROM numbers AS n1
+    WHERE NOT EXISTS (
+        SELECT 1 FROM numbers AS n2
+        WHERE n2.n < n1.n AND n2.n > 1 AND n1.n % n2.n = 0
+    )
+)
+SELECT GROUP_CONCAT(n SEPARATOR '&') AS prime_list
+FROM primes;
